fab54f3d1b0585e0aa49feb2f5c0110129c93e48
chris <chris@cutl.de>
2018-07-24 22:27:48 +0200

    added example for cron-based script

---
+++ cron.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#
+# This script can be executed by cron once a day
+#
+# crontab -e # with user that can execute this script
+#
+# 5 0 * * * $HOME/projects/cutlBrithdayReminderMail/cron.sh
+
+DIR=$HOME/projects/cutlBrithdayReminderMail
+LOG=$HOME/projects/cutlBrithdayReminderMail/log/simple.log
+
+echo " " >> $LOG
+echo " " >> $LOG
+echo "$(date) start" >> $LOG
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+
+cd $DIR
+nvm use >> $LOG
+node index.js >> $LOG
+
+echo "$(date) finished" >> $LOG
+
+
